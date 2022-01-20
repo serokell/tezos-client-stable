d1958506c1d9939b3eedb3aee4d84fa6d7ce179a
Ilona Prikule <ilona.prikule@serokell.io>
2022-01-20 16:41:29 +0100

    [DO NOT MERGE] testing PR №373

--- .buildkite/generate-tags-pipeline.sh
+++ .buildkite/generate-tags-pipeline.sh
@@ -15,7 +15,7 @@ ymlappend () {
 }
 
 # we don't bottle meta-formulas that contain only services
-formulae=("tezos-accuser-011-PtHangz2" "tezos-admin-client" "tezos-baker-011-PtHangz2" "tezos-client" "tezos-codec" "tezos-endorser-011-PtHangz2" "tezos-node" "tezos-sandbox" "tezos-signer")
+formulae=("tezos-accuser-011-PtHangz2")
 
 # tezos-sapling-params is used as a dependency for some of the formulas
 # so we handle it separately.
@@ -25,7 +25,6 @@ ymlappend "
    key: install-tsp
    agents:
      queue: \"arm64-darwin\"
-   if: build.tag =~ /^v.*/
    commands:
    - brew install --formula ./Formula/tezos-sapling-params.rb"
 
@@ -37,7 +36,6 @@ for f in "${formulae[@]}"; do
    key: build-bottle-$n
    agents:
      queue: \"arm64-darwin\"
-   if: build.tag =~ /^v.*/
    commands:
    - ./scripts/build-one-bottle.sh \"$f\"
    artifact_paths:
@@ -55,24 +53,5 @@ done
 
 ymlappend "   agents:
      queue: \"arm64-darwin\"
-   if: build.tag =~ /^v.*/
    commands:
-   - brew uninstall ./Formula/tezos-sapling-params.rb
-
- - label: Add Big Sur arm64 bottle hashes to formulae
-   depends_on:
-   - \"uninstall-tsp\"
-   if: build.tag =~ /^v.*/
-   commands:
-   - buildkite-agent artifact download \"*bottle.tar.gz\" \"Big Sur arm64\"
-   - nix-shell ./scripts/shell.nix
-       --run './scripts/sync-bottle-hashes.sh \"\$BUILDKITE_TAG\" \"Big Sur arm64\"'
-
- - label: Attach bottles to the release
-   depends_on:
-   - \"uninstall-tsp\"
-   if: build.tag =~ /^v.*/
-   commands:
-   - buildkite-agent artifact download \"*bottle.tar.gz\" .
-   - nix-shell ./scripts/shell.nix
-       --run 'gh release upload \"\$BUILDKITE_TAG\" *.bottle.*'"
+   - brew uninstall ./Formula/tezos-sapling-params.rb"
