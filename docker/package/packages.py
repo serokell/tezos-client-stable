e9cb2e1e4c73663e72af2dbce65da62cf737a045
Dmitry Laptov <dmitry.laptov@serokell.io>
2022-08-19 19:22:34 +0300

    fixup! [#453] Package for fedora and ubuntu

--- docker/package/packages.py
+++ docker/package/packages.py
@@ -447,7 +447,7 @@ def mk_rollup_packages():
         service_file = ServiceFile(
             Unit(after=["network.target"], description=f"Tezos {type} rollup node"),
             Service(
-                environment_file=f"/etc/default/tezos-{type}-rollup-node",
+                environment_file=f"/etc/default/tezos-{type}-rollup-node-{proto}",
                 environment=[f"PROTOCOL={proto}"],
                 exec_start_pre=[
                     "+/usr/bin/setfacl -m u:tezos:rwx /run/systemd/ask-password"
